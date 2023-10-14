9b1375e91ce59f223c2c20fde5aa9f0fb9e0ae7c
Kaodili Okwuaka <okwuakaella@gmail.com>
2023-10-14 16:43:17 -0500

    new changes

--- about/about.js
+++ about/about.js
@@ -2,3 +2,11 @@ import './about.css'
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: 'navbar-collapse-toggle'
   })
+  const navEl = document.querySelector('.navbar');
+// window.addEventListener('scroll', ()=> {
+//   if (window.scrollY >= 56) {
+//     navEl.classList.add('navbar-scrolled');
+//   } else if (window.scrollY < 56) {
+//     navEl.classList.remove('navbar-scrolled');
+//   }
+// });
--- src/script.js
+++ src/script.js
@@ -1,4 +1,13 @@
 import './style.css'
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: 'navbar-collapse-toggle'
-  })
+  });
+
+const navEl = document.querySelector('.navbar');
+window.addEventListener('scroll', ()=> {
+  if (window.scrollY >= 56) {
+    navEl.classList.add('navbar-scrolled');
+  } else if (window.scrollY < 56) {
+    navEl.classList.remove('navbar-scrolled');
+  }
+});
